56686f370822673803eda95326bf5cb536cfcec8
garance collette <garance@device-70.home>
2024-12-18 12:07:14 +0100

    test synthé

---
+++ Test synthé 1.rb
@@ -0,0 +1,22 @@
+use_synth :tb303
+play 38, attack: 1
+sleep 0.5
+use_synth :rodeo
+play 50, release:3
+sleep 0.25
+use_synth :mod_dsaw
+play 57, attack:0.5, release: 1
+play 52, attack:1.5, release:0.5
+sleep 1
+use_synth :tb303
+play 38, attack: 1
+use_synth :tech_saws
+play 33, attack:0.5, release:1
+use_synth :rodeo
+play 55
+play 57, amp:1
+sleep 0.25
+play 52
+sleep 0.25
+play 55
+
